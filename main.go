b3bf47071b71f6d63fcce980fe860dc7a34d089b
kazootle <katharina.salewski@icloud.com>
2024-02-14 15:50:33 +0100

    added base structs to main.go

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+// Bitte die folgenden beiden Structs in das eigene package "models" auslagern und weitere Structs hinzufügen
+
+type Rating struct {
+	Rating  int
+	Comment string
+}
+
+type Author struct {
+	Name  string
+	Email string
+}
+
 func main() {
 
 }
