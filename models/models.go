588476d2e4dac32c4bb208260ff5a5b55896217f
1921233 <83212565+1921233@users.noreply.github.com>
2024-02-15 11:08:47 +0100

    Update models.go

--- models/models.go
+++ models/models.go
@@ -1,6 +1,63 @@
 package models
 
 type Rating struct {
-	Rating  int    `json:"rating"`
-	Comment string `json:"comment"`
+	Rating  int
+	Comment string
+}
+
+type Ratings struct {
+	Interesting         Rating
+	Learning            Rating
+	Pacing              Rating
+	ExerciseDifficulty  Rating
+	Support             Rating
+	OverallSatisfaction Rating
+}
+
+type Author struct {
+	Name  string
+	Email string
+}
+
+type Feedback struct {
+	ID      string
+	Author  Author
+	Ratings Ratings
+}
+
+func validateRating(rating Rating) string {
+	// Check ratings number between 1 and 10
+	if rating.Rating < 1 || rating.Rating > 10 {
+		return "Rating must be between 1 and 10"
+		// Check comment length max 2000 characters
+	} else if len(rating.Comment) > 2000 {
+		return "Comment can contain a maximum of 2000 characters"
+	}
+	return "Valid"
+}
+
+func NewRating(rating int, comment string) (result Rating, message string) {
+	if validateRating(Rating{rating, comment}) == "Valid" {
+		message = "Erstellung erfolgreich!"
+		result = Rating{rating, comment}
+	} else {
+		message = "Bei der Erstellung ist ein Fehler Aufgetreten"
+	}
+	return
+}
+
+// Verbesserte Version der Funktion mit Pointer, hier wird bei einem Fehler <nil> zurückgegeben
+func NewRatingPointer(rating int, comment string) (result *Rating, message string) {
+	if validateRating(Rating{rating, comment}) == "Valid" {
+		message = "Erstellung erfolgreich!"
+		result = &Rating{rating, comment}
+	} else {
+		message = "Bei der Erstellung ist ein Fehler Aufgetreten"
+	}
+	return
+}
+
+func (r *Rating) ChangeRating(rating int, comment string) {
+	r.Rating = rating
+	r.Comment = comment
 }
